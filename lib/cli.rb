143925587732589e5c1c1e0b9f04d1abe2ebae33
Pat McClernan <patmccler@gmail.com>
2020-05-18 11:53:42 -0400

    start to organize cli into separate methods

--- lib/cli.rb
+++ lib/cli.rb
@@ -5,7 +5,21 @@
 
 class Cli
 
+  def initialize
+    @scraper = Scraper.new
+  end
+
   def call
-    puts "Hello, World!"
+    welcome
+    spell_by_class
+  end
+
+  def welcome
+    puts "Hello, welcome to this DND Spell lookup app!"
+    puts "Spells have both a level and a school, and can be learned by one or more classes"
+  end
+
+  def spell_by_class
+    puts "Enter a class from the list to see which spells that class knows."
   end
 end
