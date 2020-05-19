67749d4a58b5d7a6d43c56ab355592afecaeaafe
Pat McClernan <patmccler@gmail.com>
2020-05-19 17:09:42 -0400

    Shorten line in Klass.spells

--- lib/klass.rb
+++ lib/klass.rb
@@ -25,7 +25,7 @@ class Klass
   end
 
   def spells
-    klass_spells = KlassSpell.all.select{ |klass_spell| klass_spell.klass == self }
+    klass_spells = KlassSpell.all.select{ |ks| ks.klass == self }
 
     if klass_spells.empty?
       spells = Scraper.scrape_class_spells(self)
