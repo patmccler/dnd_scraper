810d002ff0712d7c12be3681db40d5689d4edf9f
Pat McClernan <patmccler@gmail.com>
2020-05-21 10:50:36 -0400

    implement find_duration method on spell scraper

--- lib/spell_scraper.rb
+++ lib/spell_scraper.rb
@@ -10,8 +10,10 @@ class SpellScraper
     spell.type = find_spell_type(tagline.text)
     spell.cast_time = find_cast_time(cast_info)
     spell.range = find_range(cast_info)
+    spell.components = find_components(cast_info)
+    spell.duration, spell.concentration = find_duration(cast_info)
 
-    binding.pry
+    # binding.pry
   end
 
   # Assumes the string is in one of the following formats:
@@ -32,4 +34,16 @@ class SpellScraper
     cast_info_p.css("strong:nth-of-type(2)")[0].next.text.strip
   end
 
+  # Text elem after third 'strong' element
+  def self.find_components(cast_info_p)
+    cast_info_p.css("strong:nth-of-type(3)")[0].next.text.strip
+  end
+
+  # Text elem after fourth 'strong' element
+  def self.find_duration(cast_info_p)
+    text = cast_info_p.css("strong:nth-of-type(4)")[0].next.text.strip
+    conc = text.include?("Concentration")
+    text = text.gsub("Concentration, ", "")
+    [text, conc]
+  end
 end
